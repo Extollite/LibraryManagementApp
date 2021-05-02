861c8929abe68f5af6cad126f0a11472107c7612
Jedrzej <42713788+Extollite@users.noreply.github.com>
2021-05-02 14:05:57 +0200

    Add thymeleaf base template

--- src/main/java/pl/rjsk/librarymanagement/LibraryManagementApplication.java
+++ src/main/java/pl/rjsk/librarymanagement/LibraryManagementApplication.java
@@ -1,16 +1,19 @@
 package pl.rjsk.librarymanagement;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 // Spring Security disabled because have to be setup
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@PropertySource("classpath:META-INF/build-info.properties")
 public class LibraryManagementApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LibraryManagementApplication.class, args);
     }
